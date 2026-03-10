897fe1a3dc74f947278fe62ca880457e2fdf1989
Charlie Sanchez <charleicharcoalz@gmail.com>
2026-03-10 14:08:52 -0400

    h

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let moralityPoints = 0;
+let currentScene = 0;
+let currentLine = 0;
+let multiChoice = false;
+let hint = false;
+let extratext = false;
 
+let charaLine = document.getElementById("mainText");
+let hintLine = document.getElementById("hintText").setAttribute("hidden", "hidden");
+let extraLine = document.getElementById("extraText").setAttribute("hidden", "hidden");
+
+let b1 = document.getElementById("option1").setAttribute("hidden", "hidden");
+let b2 = document.getElementById("option2").setAttribute("hidden", "hidden");
